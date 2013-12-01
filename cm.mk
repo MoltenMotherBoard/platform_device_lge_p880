8c555ced90aad4fb6d961c65cae9f13a1b2d4c6b
Adam77Root <adam77root@gmail.com>
2013-12-01 20:29:44 +0100

    Revert "tabs -> spaces"
    This reverts commit c4e4557a406277bc7cbd64025fe6cb96458952bd.
    
    Conflicts:
    	cm.mk
    
    Change-Id: I64214dfbbef9a4a71591708a272292715e1e75d8

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-## Specify phone tech before including full_phone
+# Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/telephony.mk)
 
 # Boot animation
@@ -17,14 +17,16 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/lge/p880/p880.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_RELEASE_NAME := Optimus4X
+# Release name and versioning
+PRODUCT_RELEASE_NAME := Optimus 4X HD
+#PRODUCT_VERSION_DEVICE_SPECIFIC :=
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p880
 PRODUCT_NAME := cm_p880
-PRODUCT_BRAND := lge
+PRODUCT_BRAND := LG
+PRODUCT_MODEL := LG-P880
 PRODUCT_MANUFACTURER := LGE
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=x3_open_eu BUILD_FINGERPRINT=lge/x3_open_eu/x3:4.1.2/JZO54K/P88020a.1e56bb4c69:user/release-keys PRIVATE_BUILD_DESC="x3_open_eu-user 4.1.2 JZO54K P88020a.1e56bb4c69 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
