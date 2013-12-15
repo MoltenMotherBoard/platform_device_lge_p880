601d014d1a45ae9b5821424bb3fbb2ce08f9a983
demetris <demetris@walla.com>
2013-12-15 23:15:12 +0200

    Update vold setup for 4.4

--- p880.mk
+++ p880.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, build/target/product/locales_full.mk)
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
