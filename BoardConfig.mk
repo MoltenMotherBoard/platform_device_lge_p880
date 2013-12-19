16e92ba4f381a68a01f11b9fa87e5b5ce7cd0f0a
demetris <demetris@walla.com>
2013-12-19 22:35:52 +0200

    Fix/Update smartdimmer. Partially update sepolicies(wip)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,7 +88,7 @@ BOARD_BATTERY_DEVICE_NAME := battery
 ifeq ($(HAVE_SELINUX),true)
 
 BOARD_SEPOLICY_DIRS += \
-    device/lge/p880/selinux
+    device/lge/p880/sepolicy
 
 BOARD_SEPOLICY_UNION += \
     file_contexts \
