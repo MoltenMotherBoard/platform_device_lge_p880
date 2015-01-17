a1531cd719c27e5ff3e7db30d11611036a5d2f2b
streambinder <davidepucci@hiddenhost.org>
2015-01-17 09:49:17 +0100

    p880: update patches method

--- patches/install.sh
+++ patches/install.sh
@@ -5,13 +5,13 @@ PATCHBASE=$(dirname "$MYABSPATH")
 CMBASE=$(readlink -f "$PATCHBASE/../../../../")
 
 for i in $(find "$PATCHBASE"/* -type d); do
-        PATCHNAME=$(basename "$i")
-        PATCHTARGET=$PATCHNAME
-        for i in $(seq 4); do
-                PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
-                if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
-        done
-        echo applying $PATCHNAME to $PATCHTARGET
-        cd "$CMBASE/$PATCHTARGET" || exit 1
-        git am -3 "$PATCHBASE/$PATCHNAME"/* || exit 1
+    PATCHNAME=$(basename "$i")
+    PATCHTARGET=$PATCHNAME
+    for i in $(seq 4); do
+        PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
+        if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
+    done
+    echo "[ $PATCHTARGET ]"
+    cd "$CMBASE/$PATCHTARGET" || exit 1
+    git am -3 "$PATCHBASE/$PATCHNAME"/* || exit 1
 done
