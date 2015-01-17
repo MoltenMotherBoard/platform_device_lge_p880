d0e27e44404824166c65813bccae8ea9258c7119
streambinder <davidepucci@hiddenhost.org>
2015-01-17 09:33:00 +0100

    p880: fix NFC

--- p880.mk
+++ p880.mk
@@ -87,7 +87,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
@@ -106,8 +105,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.primary.tegra \
     audio.a2dp.default \
-    audio.r_submix.default \
-    libaudioutils
+    audio.r_submix.default
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    log.tag.AudioFXCompat=DEBUG
 
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.use-awesome=true \
@@ -128,6 +129,15 @@ PRODUCT_PACKAGES += \
     camera.tegra \
     com.android.future.usb.accessory \
     libstagefrighthw
+
+# NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag \
+    nfc.x3
 
 # Wifi
 PRODUCT_PACKAGES += \
